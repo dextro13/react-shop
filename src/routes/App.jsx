846aaedbfb7dd9596efebcb4ca75c32519f608ec
dextro13 <42855127+dextro13@users.noreply.github.com>
2023-04-05 16:27:29 -0500

    Update App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState =useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter "/react-shop.github.io">
 			<Layout>
 				<Switch>
 					<Route exact path="/" component={Home} />
